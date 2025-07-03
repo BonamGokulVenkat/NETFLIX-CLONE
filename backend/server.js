33103aa7f5ad79ead0ad5f3bda4cbbff1471f6a7
BonamGokulVenkat <bonamgokul@gmail.com>
2025-07-03 14:52:59 +0530

    Authentication for Netflix

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import authRoutes from './routes/auth.route.js';
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
+
+const app= express();
+
+const port = ENV_VARS.PORT;
+
+app.use(express.json());
+
+app.use("/api/v1/auth",authRoutes)
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(port,()=>{
+    console.log(`running on ${port}`);
+    connectDB();
+})
+
