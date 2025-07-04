e7d20e31573caa4c4116fef9bb23b5f1aa94dde6
BonamGokulVenkat <bonamgokul@gmail.com>
2025-07-04 10:53:25 +0530

    movie routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import movieRoutes from './routes/movie.route.js'
 import authRoutes from './routes/auth.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -12,6 +12,8 @@ const port = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/movie",movieRoutes);
+
 
 app.get("/",(req,res)=>{
     res.send("hello")
